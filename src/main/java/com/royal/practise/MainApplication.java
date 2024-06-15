f62ed388223f0e189179f0eb6013f0248891b9e2
ChinmayRPatel <chinmayrpatel1309@gmail.com>
2024-06-15 11:43:38 +0530

    Initial Commit with main and dependencies

--- src/main/java/com/royal/practise/MainApplication.java
+++ src/main/java/com/royal/practise/MainApplication.java
@@ -2,11 +2,13 @@ package com.royal.practise;
 
 import org.springframework.boot.SpringApplication;
 
+
 public class MainApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(MainApplication.class, args);
+		System.out.println("Started Application *******************");
 	}
 
 }
